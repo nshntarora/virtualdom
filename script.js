bfdb24182dc00bef52a1de6816f0c88ca05482a1
Nishant Arora <na.nishantarora@gmail.com>
2017-08-27 15:05:17 +0530

    Works. Re-rendering is buggy

--- script.js
+++ script.js
@@ -20,42 +20,71 @@ var vdom = {
   ]
 };
 
-console.log(vdom);
-
 // Now we have our virtual dom, next step is to convert it into an actual DOM that the browser could render
 // For this we will be using the javascript createElement and createTextNode functions
 
-function createDOM(vdom) {
-  var newEl = document.createElement(vdom.type);
-  if (vdom.children.length > 0) {
-    vdom.children.forEach(function(element) {
-      if (element.type) {
-        var innerElement = createDOM(element);
-      } else {
-        var innerElement = document.createTextNode(element);
-      }
+function createDOM(dom) {
+  if (typeof dom === 'string') {
+    return document.createTextNode(dom);
+  }
+  var newEl = document.createElement(dom.type);
+  if (dom.children.length > 0) {
+    dom.children.forEach(function(element) {
+      var innerElement = createDOM(element);
       newEl.appendChild(innerElement);
     }, this);
   }
   return newEl;
 }
 
-function render(id, vdom) {
-  document.getElementById(id).innerHTML = '';
-  document.getElementById(id).appendChild(createDOM(vdom));
-}
-
 // By now we are able to convert our virtual dom representation in an actual dom
 // now, what we have to focus on is diffing the objects to see what has changed.
 // But, before that, let's write a small function that changes one key and updates our representation.
 
-function updateNode(vdom) {
-  vdom.children[1].children[0] = 'Nishant!';
-  return vdom;
+function initialRender() {
+  var root = document.getElementById('app');
+  updateNode(root,null, vdom);
 }
 
 function handleReload() {
-  render('app', vdom);
-  var newdom = updateNode(vdom);
-  render('app', newdom);
+  var newdom = JSON.parse(JSON.stringify(vdom));
+  newdom.type = 'span';
+  newdom.children[1].children[0] = 'Nishant!';
+  var root = document.getElementById('app');
+  updateNode(root, vdom, newdom);
+}
+
+function updateNode(parent, oldnode, newnode, index=0) {
+  if (!oldnode) {
+    parent.appendChild(createDOM(newnode));
+  } else if (!newnode) {
+    parent.removeChild(parent.childNodes[index]);
+  } else if (diffNodes(oldnode, newnode)) {
+    parent.replaceChild(createDOM(newnode), parent.childNodes[index]);
+  } else {
+    for (var i = 0; i < newnode.children.length || i < oldnode.children.length; i++) {
+      updateNode(parent.childNodes[index], oldnode.children[i], newnode.children[i], i);
+    }
+  }
+}
+
+function diffNodes(oldnode, newnode) {
+  if (oldnode && newnode) {
+    if (typeof oldnode != typeof newnode) {
+      // If the type of both nodes is different
+      // One is a text node and the other is an element
+      return true;
+    }
+    else if ((oldnode.type && newnode.type) && (oldnode.type != newnode.type)) {
+      // If both nodes are elements, but are different.
+      return true;
+    }
+    else if (oldnode != newnode) {
+      // If both nodes are string and are not equal
+      return true;
+    }
+    return false;
+  } else {
+    throw new Error('Nodes are undefined');
+  }
 }
